87ee23f8e55efa8b1324cc507c901998d5e7f53e
Akshay Karle <akshay.a.karle@gmail.com>
2015-10-02 10:53:38 -0500

    Start caching docker dir from users builds

--- cookbooks/perform_builds/attributes/default.rb
+++ cookbooks/perform_builds/attributes/default.rb
@@ -3,13 +3,17 @@ go_data_dirs = {
   '/var/lib/go-server/artifacts' => {:owner => :go, :group  => :go, :mode  => "0755"}
 }
 
+docker_dir = {
+  '/var/lib/docker' => {:owner => :root, :group => :root, :mode => "0700"}
+}
+
 git_proxy_dirs = {
   '/var/lib/git'                                 => {:owner => :git, :group => :git, :mode => "0700"},
   '/var/lib/git/.ssh'                            => {:owner => :git, :group => :git, :mode => "0700"},
 }
 
 # these live after the build completes
-default[:perform_builds][:project_dirs] = go_data_dirs.merge(git_proxy_dirs)
+default[:perform_builds][:project_dirs] = go_data_dirs.merge(git_proxy_dirs).merge(docker_dir)
 
 # these are ephemeral, will be removed by container snapshot
 default[:perform_builds][:container_dirs] = {
