1ad86cad1e6908264cbbdff17ed8a6274cbf2295
ibnc <icarter@thoughtworks.com>
2016-04-14 15:58:14 -0700

    Set pg_config bins for selected versions
    [snap/#2600] Fix pg_config for Ubuntu

--- cookbooks/perform_builds/recipes/start_database_debian.rb
+++ cookbooks/perform_builds/recipes/start_database_debian.rb
@@ -5,10 +5,19 @@ if database
   case database
     when 'postgresql'
       execute 'service postgresql start 9.2'
+      link '/usr/bin/pg_config' do
+        to '/usr/bin/pg_config-92'
+      end
     when 'postgresql93'
       execute 'service postgresql start 9.3'
+      link '/usr/bin/pg_config' do
+        to '/usr/bin/pg_config-93'
+      end
     when 'postgresql94'
       execute 'service postgresql start 9.4'
+      link '/usr/bin/pg_config' do
+        to '/usr/bin/pg_config-94'
+      end
     when 'mysql'
       service 'mysql' do
         action [:start]
