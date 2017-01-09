0a01ffd6dbd4b452a6882248a0cf8c7f63be7175
Marques Lee <marques.lee@thoughtworks.com>
2017-01-09 13:03:17 -0800

    remove .bundled-gems and .bundled-bin if .rbenv is installed in $HOME

--- cookbooks/perform_builds/recipes/dot_files.rb
+++ cookbooks/perform_builds/recipes/dot_files.rb
@@ -7,6 +7,26 @@ directory "/var/go/.bundle" do
   action (Dir.exist?("/var/go/.rbenv") ? :delete : :create)
 end
 
+directory "/var/go/.bundled-gems" do
+  recursive true
+  owner 'go'
+  group 'go'
+  mode  '0755'
+
+  action :delete
+  only_if { Dir.exist? ("/var/go/.rbenv") }
+end
+
+directory "/var/go/.bundled-bin" do
+  recursive true
+  owner 'go'
+  group 'go'
+  mode  '0755'
+
+  action :delete
+  only_if { Dir.exist? ("/var/go/.rbenv") }
+end
+
 cookbook_file "/var/go/.bundle/config" do
   source "bundle-config"
   owner 'go'
